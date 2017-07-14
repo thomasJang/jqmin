b9a6a512ddb26c03ccc143ab7da9601cc942da1a
Thomas Jang <tom@axisj.com>
2017-07-14 17:11:22 +0900

    init

---
+++ README.md
@@ -0,0 +1,19 @@
+[![npm version](https://badge.fury.io/js/jqmin.svg)](https://badge.fury.io/js/jqmin)
+[![](https://img.shields.io/npm/dm/jqmin.svg)](https://www.npmjs.com/package/jqmin)
+
+# Tiny jQuery for AX6UI
+
+### extract option
+```
+grunt --gruntfile jquery/Gruntfile.js custom:-ajax,-attributes/prop,-attributes/support,-deprecated,-effects,-wrap,-deferred,-event/focusin,-event/alias,-css/showHide,-sizzle,-core/ready,-queue 
+```
+
+### npm install
+```
+npm install ax6ui-jquery -S
+```
+
+### bower install
+```
+bower install ax6ui-jquery -S
+```
---
+++ test/test.jquery.js
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2017. tom@axisj.com
+ * - github.com/thomasjang
+ * - www.axisj.com
+ */
+
+describe('TEST', function () {
+    var myUI;
+
+    var tmpl = '<div data-test="1"></div>';
+
+    jQuery(document.body).append(tmpl);
+
+    ///
+    it('css & width', function (done) {
+        var $el = jQuery('[data-test="1"]');
+        $el.css({width: 300});
+        done($el.width() == 300 ? "" : "$el.width() = " + $el.width());
+    });
+
+    it('css & outerWidth', function (done) {
+        var $el = jQuery('[data-test="1"]');
+        $el.css({width: 300});
+        done($el.outerWidth() == 300 ? "" : "$el.outerWidth() = " + $el.outerWidth());
+    });
+
+    it('ready', function (done) {
+        jQuery(document).ready(function () {
+            done();
+        });
+    });
+
+    it('ready', function (done) {
+        $(function () {
+            done();
+        });
+    });
+
+    it('resize', function (done) {
+        jQuery(window).on("resize", function () {
+            jQuery(window).off("resize");
+            done();
+        });
+        jQuery(window).trigger("resize");
+    });
+
+
+
+});
